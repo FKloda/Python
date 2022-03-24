a75bd9e9724523b98cf1defd85af35e5bc34945b
Tural Kazimli <turalkazimli000@gmail.com>
2022-03-24 11:43:54 +0100

    Improve time efficiency, got new best results for 1000 inputs.

--- Backpack/Backpack.py
+++ Backpack/Backpack.py
@@ -43,13 +43,15 @@ def validaze_individual(individual):
             indicies.append(i)
             individual_weight += weights[i]
 
-    if individual_weight > capacity:
-        new_individual[indicies[random.randint(0, len(indicies) - 1)]] = 0
-        new_individual = validaze_individual(new_individual)
+    while individual_weight > capacity:
+        index = random.randint(0, len(indicies) - 1)
+        new_individual[indicies[index]] = 0
+        individual_weight -= weights[indicies[index]]
+        indicies.pop(index)
     return new_individual
 
 
-def mutation(population, indiv_prob=0.2, value_prob=0.3):
+def mutation(population, indiv_prob=0.2, value_prob=0.1):
     new_population = []
     for individual in population:
         if random.random() < indiv_prob:
@@ -64,8 +66,17 @@ def mutation(population, indiv_prob=0.2, value_prob=0.3):
 def create_random_population(population_size):
     population = []
     for i in range(0, population_size):
-        individual = np.random.choice([0, 1], size=(individual_size,), p=[1/2, 1/2])
-        individual = validaze_individual(individual)
+        #individual = np.random.choice([0, 1], size=(individual_size,), p=[3/4, 1/4])
+        #individual = validaze_individual(individual)
+        individual = np.zeros(individual_size)
+        weight = 0
+        while True:
+            x = random.randint(0, individual_size-1)
+            if individual[x] == 0:
+                weight += weights[x]
+                if weight > capacity:
+                    break
+                individual[x] = 1
         population.append(individual)
     return population
 
@@ -137,19 +148,19 @@ def evolution(population_size, max_generations):
     for i in range(0, max_generations):
         fitness_value = list(map(fitness, population))
 
-        best_individual = population[np.argmax(fitness_value)]
+        best_individual = copy.deepcopy(population[np.argmax(fitness_value)])
         # if fitness(best_individual) > fitness(all_time_best):
         #     all_time_best = best_individual
 
         max_fitness.append(max(fitness_value))
         selected = rulet_selection(population, fitness_value)
         # selected = tournament_selection(population, fitness_value)
-        selected[-1] = best_individual
+        selected[random.randint(0, len(selected)-1)] = best_individual
         # offsprings = one_point_crossover(selected)
         offsprings = two_point_crossover(selected)
         mutated_population = mutation(offsprings)
         population = mutated_population
-        population[-1] = best_individual
+        population[random.randint(0, len(selected)-1)] = best_individual
         print(i)
 
     fitness_value = list(map(fitness, population))
@@ -164,7 +175,7 @@ def evolution(population_size, max_generations):
 individual_size, capacity, values, weights = read_input("Data_1000.txt")
 fitness_all_time = []
 # for _ in range (1, 10):
-best, population, max_fitness = evolution(population_size=100, max_generations=100)
+best, population, max_fitness = evolution(population_size=200, max_generations=200)
 
 fitness_all_time.extend(max_fitness)
 print('best fitness: ', fitness(best))
